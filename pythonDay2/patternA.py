61b12127589738abb6ebea508cc271641f45db1d
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-11 16:39:19 +0530

    patternA added

--- pythonDay2/patternA.py
+++ pythonDay2/patternA.py
@@ -1,4 +1,16 @@
-a="  * * *  "
-b="*       *"
-c="* * * * *"
-print(a+'\n'+b+'\n'+b+'\n'+c+'\n'+b+'\n'+b+'\n'+b)
+x=[[0 for i in range(5)] for j in range(7)]
+for i in range(1,8):
+	for j in range(1,6):
+		if((i==1 or i==4) and(j==2 or j==3 or j==4)):
+			x[i-1][j-1]=1
+		elif((i!=1)and(j==1 or j==5)):
+			x[i-1][j-1]=1
+		
+
+for i in range(1,8):
+	for j in range(1,6):
+		if(x[i-1][j-1]==1):
+			print("* "),
+		else:
+			print("  "),
+	print ('\n')		
