103407946f9473a95ffd3979f37576ae2f7eabb5
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-11 17:01:02 +0530

    patternL added

---
+++ pythonDay2/patternL.py
@@ -0,0 +1,15 @@
+x=[["" for i in range(5)] for j in range(7)]
+for i in range(1,8):
+	for j in range(1,6):
+		if(j==1):
+			x[i-1][j-1]="* "
+		elif((i==7) and (j!=1)):
+			x[i-1][j-1]="* "
+		else:
+			x[i-1][j-1]="  "
+		
+
+for i in range(1,8):
+	for j in range(1,6):
+		print x[i-1][j-1],
+	print ('\n')		
