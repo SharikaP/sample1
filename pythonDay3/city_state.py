f402dd045075a5255194eed20c6617fb71ae0670
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-12 15:35:30 +0530

    city & state code added

---
+++ pythonDay3/city_state.py
@@ -0,0 +1,4 @@
+dict={'pune':'maharashtra', 'hyderabad':'telangana', 'chennai':'tamilnadu'}
+#print(dict[raw_input("Enter a city to know the state in which city is located: ")])
+inv_dict = {v: k for k, v in dict.iteritems()}
+print(inv_dict[raw_input("Enter a state to know the city located in it: ")])
