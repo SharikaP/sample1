5802f595b3e0c1de71cc2932db406050c7432ef3
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-17 16:54:44 +0530

    list using classes

---
+++ assessment/q18.py
@@ -0,0 +1,11 @@
+class list():
+	def __init__(self):
+		self=[]
+	def append(self, a):
+		return self.append(a)
+	def delete(self, b):
+		return self.remove(b)
+	def display(self):
+		return (self)
+
+		
