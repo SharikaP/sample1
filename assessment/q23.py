a05d788a29fc54664de2086cde2fcd8ff88133f1
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-17 10:41:32 +0530

    appending string to a file

--- assessment/q23.py
+++ assessment/q23.py
@@ -1,5 +1,5 @@
-filename=raw_input("Enter file name to be opened: ")
-fopen=open(fileName, "a")
-a=raw_input("Enter the string to be appended: ")
-fopen.write(a)
-fopen.close()
+filename=raw_input("Enter file name to be opened: ") #inputting the file name
+fopen=open(fileName, "a") #opening the file to be changed
+a=raw_input("Enter the string to be appended: ") #inputting the string to be appended
+fopen.write(a) #appending that string to the file
+fopen.close() #closing the file
