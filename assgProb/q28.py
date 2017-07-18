46c3828caa5365c89b75f8276310dfa8f2f9bbf9
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-18 20:05:10 +0530

    validity of passwords added

--- assgProb/q28.py
+++ assgProb/q28.py
@@ -7,7 +7,7 @@ for password in li:
 		b=any(x.isupper() for x in password) #checking for the presence of atleast 1 uppercase letter
 		c=any(x.islower() for x in password) #checking for the presence of atleast 1 lowercase letter
 		d=all([len(password)>=6,len(password)<=16]) #criteria for length of a password
-		e=any([password.find('$'), password.find('#'), password.find('@')]) #checking for the presence of a special character
+		e=any(x in "[$#@]" for x in password) #checking for the presence of a special character
 		if all([a,b,c,d,e])==True:
 			l1.append(password)
 print (l1)
