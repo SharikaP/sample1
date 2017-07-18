b93e4c964d56c7e46cfcb261412e923c1d2c04e7
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-18 17:29:29 +0530

    binary search added

---
+++ assgProb/q23.py
@@ -0,0 +1,14 @@
+import math
+def bin_search(li, element):
+	bottom=0
+	top=len(li)-1
+	index=-1
+	while top>bottom and index==-1:
+		mid=int(math.floor((top+bottom)/2.0))
+		if li[mid]==element:
+			index=mid
+		elif li[mid]>element:
+			top=mid-1
+		else:
+			bottom=mid+1
+	return index
