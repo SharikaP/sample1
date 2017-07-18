b935c63603cbacaf1ef2aedaf9059463a412ad1f
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-18 17:50:12 +0530

    class gender added

---
+++ assgProb/q20.py
@@ -0,0 +1,9 @@
+class Person:
+	def getGender(self):
+		return "Unknown"
+class Male(Person):
+	def getGender(self):
+		return ("Male")
+class Female(Person):
+	def getGender(self):
+		return ("Female")
