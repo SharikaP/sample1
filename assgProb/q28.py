10ee9b7f83173bf95503dd0e242f2fb97d1bd5f3
SharikaP <saiharika.padmanabhuni@quantiphi.com>
2017-07-18 19:51:08 +0530

    validity of passwords

---
+++ assgProb/q28.py
@@ -0,0 +1,13 @@
+
+lis=raw_input("enter a list of passwords: ")
+li=lis.split(',')
+l1=[]
+for password in li:
+		a=any(x.isdigit() for x in password) #checking for the presence of atleast 1 digit
+		b=any(x.isupper() for x in password) #checking for the presence of atleast 1 uppercase letter
+		c=any(x.islower() for x in password) #checking for the presence of atleast 1 lowercase letter
+		d=all([len(password)>=6,len(password)<=16]) #criteria for length of a password
+		e=any([password.find('$'), password.find('#'), password.find('@')]) #checking for the presence of a special character
+		if all([a,b,c,d,e])==True:
+			l1.append(password)
+print (l1)
